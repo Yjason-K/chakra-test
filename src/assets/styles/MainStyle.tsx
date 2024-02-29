8942423b023c8639add41a471ca99816ed24fa7a
박지수 <100174183+wlrlttn@users.noreply.github.com>
2024-02-29 13:26:54 +0900

    emotion styled 추가 (css, styled)

--- src/assets/styles/MainStyle.tsx
+++ src/assets/styles/MainStyle.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 
 export const div = css({
     margin: '20px 0'
@@ -14,3 +15,14 @@ export const button = css({
     },
 });
 
+export const GitButton = styled.button`
+    display: flex;
+    align-content: center;
+    right: 0;
+    top: 5rem;
+    padding: 1rem;
+    background: #ffffff;
+    box-shadow: 0px 1px 9px rgba(10, 31, 68, 0.1),
+        0px 0px 1px rgba(10, 31, 68, 0.08), 0px 8px 10px rgba(10, 31, 68, 0.1);
+    border-radius: 4px;
+`;
